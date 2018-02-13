ba10768a12f53261364c2e1695a97235e2175c72
Rafal Ozog <rafalozog@gmail.com>
2018-02-13 15:26:27 +0000

    First commit
    Beginning of a simple game project. The idea is to engage player into shooting different objects flying through the screen.

---
+++ Android Studio Project/app/src/main/java/ozog/development/xtremedefender/Bullet.java
@@ -0,0 +1,17 @@
+package ozog.development.xtremedefender;
+
+import android.graphics.drawable.Drawable;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Bullet {
+
+    public void generateBullet() {
+
+
+    }
+
+}
