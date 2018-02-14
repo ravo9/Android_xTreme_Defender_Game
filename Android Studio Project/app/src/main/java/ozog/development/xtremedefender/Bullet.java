58b83edacc22d0e4e8e87918de0ecafd7f744127
Rafal Ozog <rafalozog@gmail.com>
2018-02-14 14:02:31 +0000

    Movement mechanism changed
    Movement mechanism is now related to timer, what allowes the bullets be touchable now. I have also removed the title bar and implemented stopwatch.

--- Android Studio Project/app/src/main/java/ozog/development/xtremedefender/Bullet.java
+++ Android Studio Project/app/src/main/java/ozog/development/xtremedefender/Bullet.java
@@ -4,10 +4,16 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Point;
 import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.util.Log;
+import android.view.View;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Random;
@@ -18,6 +24,7 @@ public class Bullet {
     private Drawable d;
     private RelativeLayout rl;
     private ImageView iv;
+    private static float speed;
     private static Random generator;
     private static int screenWidth;
     private static int screenHeight;
@@ -26,9 +33,10 @@ public class Bullet {
         screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
         screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
         generator = new Random();
+        speed = 1;
     }
 
-    public Bullet( RelativeLayout rl, Context context, Drawable d ) {
+    public Bullet(RelativeLayout rl, final Context context, Drawable d ) {
         this.rl = rl;
         iv = new ImageView(context);
 
@@ -36,23 +44,56 @@ public class Bullet {
         LayoutParams lp = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
         iv.setLayoutParams(lp);
 
+        iv.setScaleX(3);
+        iv.setScaleY(3);
+
+        iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iv.setVisibility(View.GONE);
+                xTremeDefender.updateScore();
+            }
+        });
+
         this.d = d;
 
         iv.setImageDrawable(d);
 
         origin = generateOrigin();
+        iv.setX(origin.x);
+        iv.setY(origin.y);
+
         target = generateTarget(getOriginAreaNumber(origin));
 
-        // It would be better to make it relative instead of fixed.
-        TranslateAnimation moveBullet = new TranslateAnimation( origin.x, target.x, origin.y, target.y );
-        moveBullet.setDuration(2000);
-        moveBullet.setFillAfter(true);
-
-        iv.startAnimation(moveBullet);
         rl.addView(iv);
 
-
         // Destroy!
+    }
+
+    public void updatePosition() {
+
+        // Movement
+        float x = this.iv.getX();
+        float y = this.iv.getY();
+
+        float targetX = this.target.x;
+        float targetY = this.target.y;
+
+        float ratio = Math.abs(target.x - x) / Math.abs(target.y - y);
+
+        if ( this.target.x < x )
+            this.iv.setX( x - speed * ratio );
+        else if ( this.target.x > x )
+            this.iv.setX( x + speed * ratio );
+
+        if ( this.target.y < y )
+            this.iv.setY( y - speed * 1/ratio );
+        else if ( this.target.y > y )
+            this.iv.setY( y + speed * 1/ratio );
+
+        // Rotation
+        this.iv.setRotation(this.iv.getRotation() + 8);
+
     }
 
     private Point generateOrigin() {
--- Android Studio Project/app/src/main/java/ozog/development/xtremedefender/xTremeDefender.java
+++ Android Studio Project/app/src/main/java/ozog/development/xtremedefender/xTremeDefender.java
@@ -1,20 +1,24 @@
 package ozog.development.xtremedefender;
 
+import android.app.ActionBar;
 import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.Point;
 import android.graphics.drawable.Drawable;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.view.animation.TranslateAnimation;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.RelativeLayout;
-import android.widget.RelativeLayout.LayoutParams;
+import android.widget.TextView;
+
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class xTremeDefender extends AppCompatActivity {
 
@@ -22,14 +26,30 @@ public class xTremeDefender extends AppCompatActivity {
     RelativeLayout rl;
     Context c;
 
-    int score = 0;
+    static TextView textView1;
+    static TextView textView2;
 
-    //Button btn1 = findViewById(R.id.btn1);
+    int centiseconds = 0;
+    int seconds = 0;
+    int minutes = 0;
+
+    ArrayList<Bullet> bullets;
+
+    Timer timer;
+    Timer timer2;
+    Timer timer3;
+
+    Handler handler = new Handler();
+    static int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_x_treme_defender);
+
+        timer = new Timer();
+        timer2 = new Timer();
+        timer3 = new Timer();
 
         try {
             InputStream stream = getAssets().open("bullet1.png");
@@ -40,23 +60,100 @@ public class xTremeDefender extends AppCompatActivity {
 
         rl = findViewById(R.id.relativeLayout1);
         c = getApplicationContext();
+        textView1 = findViewById(R.id.textView1);
+        textView2 = findViewById(R.id.textView2);
+
+        bullets = new ArrayList<>();
     }
 
     public void newGame( View v) {
 
         score = 0;
-        new Bullet( rl, c, d );
-        //btn1.setVisibility(View.GONE);
 
-        /*while ( score <= 5 ) {
-            new Bullet( rl, c, d );
-            try {
-                Thread.sleep( 4000 );
-            }
-            catch( InterruptedException e ) {
-                System.out.print(e.getMessage());
-            }
-        }*/
+        Button btn1 = findViewById(R.id.btn1);
+        btn1.setVisibility(View.GONE);
 
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run () {
+                        update();
+                    }
+                });
+            }
+        }, 0, 10);
+
+        timer2.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run () {
+                        bullets.add(new Bullet( rl, c, d ));
+                    }
+                });
+            }
+        }, 0, 3500);
+
+        timer3.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run () {
+                        updateTime();
+                    }
+                });
+            }
+        }, 0, 10);
+
+    }
+
+    public void update() {
+        for ( Bullet b: bullets ) {
+            b.updatePosition();
+        }
+    }
+
+    public void updateTime() {
+
+        if ( centiseconds < 99)
+            centiseconds++;
+        else {
+            centiseconds = 0;
+
+            if ( seconds < 59)
+                seconds++;
+            else {
+                seconds = 0;
+
+                if ( minutes < 59)
+                    minutes++;
+                else
+                    minutes = 0;
+            }
+        }
+
+        String min = Integer.toString(minutes);
+        if ( minutes < 10 )
+            min = "0" + min;
+
+        String sec = Integer.toString(seconds);
+        if ( seconds < 10 )
+            sec = "0" + sec;
+
+        String centsec = Integer.toString(centiseconds);
+        if ( centiseconds < 10 )
+            centsec = "0" + centsec;
+
+        textView2.setText( ( ( ( min + ":" ) + sec ) + ":" ) + centsec );
+    }
+
+    public static void updateScore() {
+        score++;
+        textView1.setText( "Score: " + score );
     }
 }
